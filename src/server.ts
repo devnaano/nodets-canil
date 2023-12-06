797d4120b5dc15ee60ef0693a44c67dc7f504588
devnaano <naano.dev@gmail.com>
2023-12-06 16:17:58 -0300

    creating and preparing Controllers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -14,9 +15,11 @@ server.engine('mustache', mustache());
 server.use(express.static(path.join(__dirname, '../public')));
 
 //ROTAS
+server.use(mainRoutes);
 
-
-
+server.use((req, res) => {
+    res.send('home');
+})
 //ROTAS
 
 server.listen(process.env.PORT);
